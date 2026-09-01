ef9afd13e7f28b5c88bf0bb3a64eb788ac4db292
yeluofengliang <71509214+yeluofengliang@users.noreply.github.com>
2026-09-01 11:52:32 +0800

    Delete PotatoVN.App.Plugin.ScrapeControl directory

--- PotatoVN.App.Plugin.ScrapeControl/Controls/Widgets/VStack.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.UI.Xaml.Controls;
-
-namespace PotatoVN.App.Plugin.ScrapeControl.Controls.Widgets;
-
-/// <summary>统一间距的纵向容器。</summary>
-public class VStack : StackPanel
-{
-    public VStack() => Spacing = 13;
-}
